f4b9953d52ad4f9b84095c9e3af2fbc77633ad13
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-15 19:12:37 -0700

    iterated over devices in usb list
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod usb_reader;
 use processor::Processor;
 use usb_reader::UsbReader;
 fn main() {
-    let new_reader = UsbReader::new("Olympus");
+    let new_reader = UsbReader::find_device("Olympus");
     let serial_num = new_reader.get_serial_num();
 
     println!("Hello, world!");
--- src/usb_reader.rs
+++ src/usb_reader.rs
@@ -1,21 +1,32 @@
 use std::fmt::Error;
 
 use rusb::DeviceDescriptor;
+use rusb::DeviceHandle;
 
 pub struct UsbReader<'a> {
     device_name: &'a str,
 }
 
 impl<'a> UsbReader<'a> {
-    pub fn new(device_name: &'a str) -> Self {
+    // find_device - finds the specific device from the device list.
+    // not implemented yet.
+    pub fn find_device(device_name: &'a str) -> Self {
+        println!("not implemented yet");
         Self { device_name }
     }
 
+    // get_serial_num - gets the serial number from the device selected.
     pub fn get_serial_num(&self) -> Result<String, String> {
         let device_list = match rusb::devices() {
             Ok(devices) => devices,
             Err(why) => panic!("could not find devices: {why}"),
         };
+
+        let devices_opened: Vec<DeviceHandle<_>> = device_list
+            .iter()
+            .map(|x| x.open().expect("could not open device"))
+            .collect();
+        println!("{:?}", devices_opened);
         Ok("not implemented".to_string())
     }
 
